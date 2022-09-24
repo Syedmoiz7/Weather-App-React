d9ff82667a330a5277d1119b3f5ccce542828631
Syedmoiz7 <syedzada7866@gmail.com>
2022-09-24 17:07:37 +0500

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./components/weather"
 
 function App() {
   return (
--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,21 +1,40 @@
 import './index.css';
 import { useState, useEffect } from 'react';
-import axios from 'axios '
+import axios from "axios" 
 
 function Weather() {
     const [weatherData, setWeatherData] = useState(null);
     const [cityName, setCityName] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=e0f99c494c2ce394a18cc2fd3f100543`)
+    }
+
 
 
 
     return (
         <div className=''>  
             <form onSubmit={submitHandler}>
-
+                <input type="text" className='userInput' required placeholder='Enter Your City Name' onChange={(e => (setCityName(e.target.value)))}
+                 />
             </form>
+            {(weatherData === null) ? null : 
+            <div className="mainContainer">
+                <div className="renderWeather">
+                    div
+                </div>
+            </div>
+
+            }
+
+
         </div>
     )
-
 }
+
+export default Weather;
 
 
