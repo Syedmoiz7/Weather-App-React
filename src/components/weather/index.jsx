1a2ee0f66f579ce3140fecece2144a67bd65c46a
Syedmoiz7 <syedzada7866@gmail.com>
2022-09-24 16:09:41 +0500

    2nd commit

---
+++ src/components/weather/index.jsx
@@ -0,0 +1,19 @@
+import './index.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios '
+
+function Weather() {
+    const [weatherData, setWeatherData] = useState(null);
+    const [cityName, setCityName] = useState("");
+
+
+
+    return (
+        <div className=''>
+
+        </div>
+    )
+
+}
+
+
